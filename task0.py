ad7899e2104ebff19d580b6c980c9a3f686afcf1
Nagamdii <nn600888@dal.ca>
2020-02-20 09:58:07 -0800

    Implementation completed

--- task0.py
+++ task0.py
@@ -1,2 +1,10 @@
-a = [i for i in range(101)]
+a = [i for i in range(1,101)]
+#print(a)
+for b,c in enumerate (a):
+	if c%15==0:
+		a[b] = 'fizzbuzz'
+	elif c%5==0:
+		a[b] = 'buzz'
+	elif c%3==0:
+		a[b] = 'fizz'
 print(a)
